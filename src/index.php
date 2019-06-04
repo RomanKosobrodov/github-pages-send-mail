323347ec8bf8013a4d378f6ddf792d5273ae2df5
Roman Kosobrodov <roman@kosobrodov.net>
2019-06-04 10:54:05 +1000

    renamed directory; fixed path in require

--- src/index.php
+++ src/index.php
@@ -1,12 +1,12 @@
 <?php
-require 'vendor/autoload.php'; 
-$email = new \SendGrid\Mail\Mail(); 
+require('../vendor/autoload.php');
+$email = new SendGrid\Mail\Mail(); 
 $email->setFrom("test@example.com", "Example User");
 $email->setSubject("GitHub Pages Contact");
 $email->addTo(getenv('RECIPIENT_EMAIL'), getenv('RECIPIENT_NAME'));
 $email->addContent("text/plain", "contact form content goes here");
 
-$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+$sendgrid = new SendGrid(getenv('SENDGRID_API_KEY'));
 try {
     $response = $sendgrid->send($email);
     print $response->statusCode() . "\n";
