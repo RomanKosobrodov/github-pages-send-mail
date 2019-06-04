ffa25fdb1cc34a918a29e51643cf578354f310c1
Roman Kosobrodov <roman@kosobrodov.net>
2019-06-04 11:50:27 +1000

    added contact form example

--- src/index.php
+++ src/index.php
@@ -1,17 +1,26 @@
 <?php
 require('../vendor/autoload.php');
-$email = new SendGrid\Mail\Mail(); 
-$email->setFrom("test@example.com", "Example User");
-$email->setSubject("GitHub Pages Contact");
-$email->addTo(getenv('RECIPIENT_EMAIL'), getenv('RECIPIENT_NAME'));
-$email->addContent("text/plain", "contact form content goes here");
+if(isset($_POST['submit'])){
+    $sender = $_POST['sender'];
+    $emailAddress = $_POST['email'];
+    $message = $_POST['message'];
+    $redirectURL = $_POST['redirect'];
 
-$sendgrid = new SendGrid(getenv('SENDGRID_API_KEY'));
-try {
-    $response = $sendgrid->send($email);
-    print $response->statusCode() . "\n";
-    print_r($response->headers());
-    print $response->body() . "\n";
-} catch (Exception $e) {
-    echo 'Caught exception: '. $e->getMessage() ."\n";
+    $email = new SendGrid\Mail\Mail(); 
+    $email->setFrom($emailAddress, $sender);
+    $email->setSubject("Contact Form sent from GitHub Pages");
+    $email->addTo(getenv('RECIPIENT_EMAIL'), getenv('RECIPIENT_NAME'));
+    $email->addContent("text/plain", $message);
+
+    $sendgrid = new SendGrid(getenv('SENDGRID_API_KEY'));
+    try {
+        $response = $sendgrid->send($email);
+        header('Location: ' . $redirectURL);
+        #print $response->statusCode() . "\n";
+        #print_r($response->headers());
+        #print $response->body() . "\n";
+    } catch (Exception $e) {
+        echo 'Caught exception: '. $e->getMessage() ."\n";
+    }
 }
+?>
